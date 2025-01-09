9b08a75da0720c01101fdbfef44346524df398c7
bernardl24 <thirdybernardo2410@gmail.com>
2025-01-09 15:06:04 +0800

    push update delete and add

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ type Todo = Schema['Todo']['type'];
 type UserTodo = Schema['UserTodo']['type'];
 import './App.css';
 //import Navbar from './components/Navbar/Navbar';
-import AOS from 'aos';
+
 // import Logo from './components/Logo/Logo';
 // import Header from './components/Header/Header';
 // import Banner from './components/Banner/Banner';
